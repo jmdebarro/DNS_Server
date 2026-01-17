246918dfc612b455b5870042deb890c3674e46b3
Jonathan DeBarro <jdebarro@calpoly.edu>
2026-01-17 15:29:17 -0500

    Added gitignore and setup socket pointed to by fd

--- main.c
+++ main.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include "secrets.h"
 
 #define SUCCESS 0
 #define FAILURE 1
-#define PORT 53
+#define UDP_PORT 53
+
+/* 
+Redirect DNS UDP to PI
+Block ads
+Send back to router to all devices on network
+*/
 
 int main() {
+    struct sockaddr_in address;
     int socket_fd;
+    int opt = 1;
 
     if (socket_fd = socket(AF_INET, SOCK_DGRAM, 0) < 0) {
-        printf("Failed to create socket");
-        return FAILURE;
+        perror("Failed to create socket");
+        exit(EXIT_FAILURE);
     }
+
+    // Forcefully attaching socket to the port 53
+    if (setsockopt(socket_fd, SOL_SOCKET,
+                   SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_port(UDP_PORT);
+    address.sin_addr.s_addr = INADDR_ANY;
 
     return SUCCESS;
 }
